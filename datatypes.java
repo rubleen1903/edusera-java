6144b6752fc3dbf36bc5a7274b055dd486bb4b52
rubleen1903 <rubleen16@gmail.com>
2021-09-23 23:54:45 +0530

    updated the code

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes{
+    public static void main(String[] args) {
+        // boolean b = false;
+        // if(b == true)
+        // {
+        //     System.out.println("Hi Edusera");
+        // }
+        // else{
+        //     System.out.println("Hi Team");
+        // }
+
+        float a = 2.3f;
+        double b = 7.0;
+        System.out.println(a/b);
+    }
+}
